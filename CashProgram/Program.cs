5c4ac715f3c6d78676183c6d946965da737586a5
Gusabavo <gustavo.sk.caxias@gmail.com>
2024-05-02 14:00:09 -0300

    Adicionar arquivos de projeto.

---
+++ CashProgram/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CashProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
